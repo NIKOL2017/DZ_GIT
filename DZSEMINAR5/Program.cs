448c0382666385567e154bfbd38b06d2a8527782
Lybov <kolme3@mail.ru>
2023-12-17 22:30:21 +0300

    первая задача

---
+++ DZSEMINAR5/Program.cs
@@ -0,0 +1,53 @@
+﻿// Домашнее задание к 5-му семинару "Двумерные массивы"
+//Задайте двумерный массив из целых чисел. Пользователь вводит 
+// позиции элемента, выводится элемент либо сообщение , что его нет
+
+ int[,] array = new int[3,4];
+
+  void CreateArray() // Заполняем массив
+   {
+       for (int i = 0; i < array.GetLength(0); i++)
+       {
+           for (int j = 0; j < array.GetLength(1); j++)
+           {
+               array[i,j] = new Random().Next(1,10);
+           }
+       }
+   }
+CreateArray();
+   void PrintArray() // Выводим массив
+   {
+       for (int i = 0; i < array.GetLength(0); i++)
+       {
+           for (int j = 0; j < array.GetLength(1); j++)
+           {
+
+               System.Console.Write(array[i,j] + " ");
+           }
+           System.Console.WriteLine();
+       }
+       System.Console.WriteLine();
+   }
+   PrintArray();
+   
+Console.WriteLine("Введите номер строки");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1<0|num1>array.GetLength(0)|num2<0|num2>array.GetLength(1))
+{
+    Console.WriteLine("Такого элемента нет ");
+}
+else
+    for (int i = 0; i < array.GetLength(0); i++)
+       {
+         for (int j = 0; j < array.GetLength(1); j++)
+         {
+         if(i==num1&&j==num2)
+         {
+             Console.Write("Элемент найден "+ array[num1,num2] + " " );       
+         } 
+         } 
+       }     
+
+  
