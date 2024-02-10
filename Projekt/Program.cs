30bb6d8db76148d07e498535c23183f04f4e0e9f
Lybov <kolme3@mail.ru>
2024-02-10 19:47:58 +0300

    Добавили комментариев к коду

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -3,16 +3,18 @@
 // формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении использовать массивы.
+Console.WriteLine("Исходные данные");
+Console.WriteLine("123,235,hello,world,res");
 string[] arrayOrig = new string[5] {"123", "235", "hello", "world", "res"};// Исходный массив
 string[] arrayResult = new string[arrayOrig.Length];  // Результирующий массив
-void ResultArr(string[] arrayOrig, string[] arrayResult)
+void ResultArr(string[] arrayOrig, string[] arrayResult) 
 {
     int count = 0;
-    for (int i = 0; i < arrayOrig.Length; i++)
+    for (int i = 0; i < arrayOrig.Length; i++) // перемещение по элементам исходного массива
     {
-    if(arrayOrig[i].Length <= 3)
+    if(arrayOrig[i].Length <= 3) //проверка условия длинны каждого элемента
         {
-        arrayResult[count] = arrayOrig[i];
+        arrayResult[count] = arrayOrig[i]; // заполнение результирующего массива
         count++;
         }
     }
@@ -23,7 +25,9 @@ void PrintArr(string[] array)
     {
         Console.Write($"{array[i]} ");
     }
+    
     Console.WriteLine();
 }
 ResultArr(arrayOrig, arrayResult);
-PrintArr(arrayResult);
+Console.WriteLine("Вот такой массив на выходе");
+PrintArr(arrayResult);// Вывод на консоль результатов
